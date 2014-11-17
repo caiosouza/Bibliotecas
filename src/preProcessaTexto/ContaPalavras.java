3d29fe70aa2defe57637954b5afc52e2daf90baa
Caio <caios7@gmail.com>
2014-11-17 11:30:40 -0200

    add project
    adding all files from work

---
+++ src/preProcessaTexto/ContaPalavras.java
@@ -0,0 +1,348 @@
+package preProcessaTexto;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+//import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import util.Util;
+ 
+public class ContaPalavras {
+	
+	public static final int frequenciaMin = 0;
+
+    public static void main(String[] args) throws Exception {
+ 
+    	ContaPalavras contaPalavras = new ContaPalavras();
+    	contaPalavras.exec(args);
+    }
+    
+    public void exec(String[] args){
+    	
+    	List<String> linhas = new ArrayList<String>();
+    	
+    	try {
+			linhas = leArquivo(args);
+		} catch (Exception e) {
+			System.out.println("[ERROR] Erro ao ler arquivo");
+			System.exit(-1);
+		}
+    	Map<String,Integer> mapPalavras = contaFrequenciaNgrama(linhas, frequenciaMin, null,1);
+    	imprimeFrequencias(mapPalavras);
+    }
+    
+	public List<String> leArquivo(String[] args) throws Exception{
+    	
+    	List<String> linhas = new ArrayList<String>();
+    	
+    	String nomeArq;     
+        
+        //testa se nome do arq. texto foi passado na chamada do programa
+        
+        if (args.length != 1) {
+        	Scanner s = new Scanner(System.in);
+        	System.out.println("Entre com o nome do arquivo.");
+        	nomeArq = s.next();
+        	
+        }
+        else {
+        	nomeArq = args[0];
+        }
+        nomeArq = "data/100001000044.txt";
+     
+        //abre o arquivo
+       
+        BufferedReader txtBuffer = new BufferedReader(new InputStreamReader(
+        	    new FileInputStream(nomeArq), "UTF-8"));
+        
+        //pega a primeira linha do arquivo
+        String curLine; //recebe cada linha lida do arquivo texto
+        
+        curLine = txtBuffer.readLine();
+         
+        while (curLine != null) {
+        	linhas.add(curLine);
+            //pega a proxima linha do arquivo
+            curLine = txtBuffer.readLine();
+        }
+         
+        txtBuffer.close();
+ 
+    	return linhas;
+    }
+    	
+	
+	public static Map<String, Integer> filtraFrequencia(Map<String, Integer> mapOriginal, int minFrequencia){
+		
+		Map<String, Integer> mapFiltrado = new HashMap<String,Integer>();
+		
+		for (Map.Entry<String, Integer> entry : mapOriginal.entrySet()) {
+    		if(entry.getValue() > minFrequencia){
+    			mapFiltrado.put(entry.getKey(), entry.getValue());
+    		}
+			
+    	}
+		return mapFiltrado;
+	}
+	
+//	public static Map<String, Integer> contaFrequencia(List<String> linhas, int minFrequencia, Set<String> filtroTermos, int ngrama) {
+//		
+//		if (ngrama == 1){
+//			return contaFrequencia(linhas, minFrequencia, filtroTermos);
+//		}
+//		else return contaFrequenciaNgrama(linhas, minFrequencia, filtroTermos, ngrama);
+//	}
+	
+	public static Map<String, Integer> contaFrequenciaNgrama(List<String> linhas, int minFrequencia, Set<String> filtroTermos, int ngrama) {
+		//TODO
+		//Substituir o conta frequencia anterior por este depois de verificar se os resultados se mantem
+		//Substituir e rodar novamente e verificar se os resultados se mantem
+		//Rodar usando bigrama
+		
+		Map<String,Integer> mapPalavras = new HashMap<String,Integer>(); 
+		
+		for (String curLine : linhas) {
+    		
+			curLine = curLine.toLowerCase().replaceAll("[.,:;<>{}|_1234567890!@#$%&*()/?+=-]", " ");
+			String [] tokens = Util.limpaTokens(curLine.split(" "));
+			
+			for (int i = 0; i < (tokens.length - ngrama + 1); i++) {
+				String termoAtual = tokens[i];
+				for (int j = 1; j < ngrama; j++) {
+					termoAtual = termoAtual + " " + tokens[i+j]; 
+				}
+	
+				int freqTermoAtual = 0;
+				if (mapPalavras.containsKey(termoAtual)){
+					freqTermoAtual = mapPalavras.get(termoAtual);
+				}
+				//atualiza a frequencia
+				freqTermoAtual = freqTermoAtual + 1;
+				
+				if((filtroTermos == null) || (filtroTermos.contains(termoAtual))){
+					mapPalavras.put(termoAtual, freqTermoAtual);
+				}
+			}		
+		}
+        
+    	return filtraFrequencia(mapPalavras, minFrequencia);
+	}
+
+	public static Map<String, Integer> contaFrequenciaNgrama(String linha, int minFrequencia, Set<String> filtroTermos, int ngrama) {
+		
+		List<String> linhas = new ArrayList<String>();
+		linhas.add(linha);
+		return contaFrequenciaNgrama(linhas, minFrequencia, filtroTermos, ngrama);
+	}	
+	
+	
+//	public static Map<String, Integer> contaFrequencia(String linha, int minFrequencia, Set<String> filtroTermos) {
+//		
+//		List<String> linhas = new ArrayList<String>();
+//		linhas.add(linha);
+//		return contaFrequencia(linhas, minFrequencia, filtroTermos);
+//	}	
+		
+//	public static Map<String, Integer> contaFrequencia(List<String> linhas, int minFrequencia, Set<String> filtroTermos) {
+//
+//		Map<String,Integer> mapPalavras = new HashMap<String,Integer>(); 
+//    	for (String curLine : linhas) {
+//    		String minusculo = curLine.toLowerCase();
+//            
+//            //limpa removendo acentos, cedilhas etc...
+//            minusculo = limpacaracteres(minusculo);
+//            minusculo = removeNumeros(minusculo);
+//        	Pattern p = Pattern.compile("([a-záéíóúçãõôê]+)");
+//            //Pattern p = Pattern.compile("(\\d+)|([a-z]+)");
+//            Matcher m = p.matcher(minusculo);
+//            
+//            while(m.find())
+//            {
+//            	String token = m.group(); //pega um token
+//            	if(token.length()>1){
+//	            	Integer freq = mapPalavras.get(token); //verifica se esse token ja esta no mapa   
+//	            	//totalTermos = totalTermos +1; 
+//	            	if(filtroTermos!= null) {
+//	            		if (filtroTermos.contains(token)){
+//	            			if (freq != null) { //se palavra existe, atualiza a frequencia
+//	      	            	  mapPalavras.put(token, freq+1);
+//	      	            	}
+//	      	                else { // se palavra nao existe, insiro com um novo id e freq=1.
+//	      	                    mapPalavras.put(token,1);
+//	      	                }
+//	            		}
+//	            	} else {
+//		            	if (freq != null) { //se palavra existe, atualiza a frequencia
+//		            	  mapPalavras.put(token, freq+1);
+//		            	}
+//		                else { // se palavra nao existe, insiro com um novo id e freq=1.
+//		                    mapPalavras.put(token,1);
+//		                }
+//	            	}
+//            	}
+//            }
+//        }
+//
+//        return filtraFrequencia(mapPalavras,minFrequencia);
+//    }
+
+//	public static Map<String, Integer> contaPresenca(List<String> linhas, int minFrequencia, Set<String> filtroTermos, int ngrama) {
+//		
+//		if (ngrama == 1){
+//			return contaPresenca(linhas, minFrequencia, filtroTermos);
+//		}
+//		else return contaPresencaNgrama(linhas, minFrequencia, filtroTermos, ngrama);
+//	}
+	
+	public static Map<String, Integer> contaPresencaNgrama(List<String> linhas, int minFrequencia, Set<String> filtroTermos, int ngrama) {
+		
+		Map<String,Integer> mapPalavrasConsolidado = new HashMap<String,Integer>(); 
+		if (ngrama == 1){
+			return contaPresenca(linhas, minFrequencia, filtroTermos);
+		}
+		else return mapPalavrasConsolidado;//contaPresencaNgrama(linhas, minFrequencia, filtroTermos, ngrama);
+	
+		
+		//Map<String,Integer> mapPalavrasConsolidado = new HashMap<String,Integer>(); 
+    	// TODO implementar metodo
+		//return mapPalavrasConsolidado;
+	}
+
+	public static Map<String, Integer> contaPresenca(List<String> linhas, int minFrequencia, Set<String> filtroTermos) {
+			
+		Map<String,Integer> mapPalavrasConsolidado = new HashMap<String,Integer>(); 
+    	
+		
+		for (String curLine : linhas) {
+			
+			Map<String,Integer> mapPalavrasAtual = new HashMap<String,Integer>(); 
+			String minusculo = curLine.toLowerCase();
+            
+            //limpa removendo acentos, cedilhas etc...
+            minusculo = limpacaracteres(minusculo);
+            minusculo = removeNumeros(minusculo);
+        	Pattern p = Pattern.compile("([a-záéíóúçãõôê]+)");
+            //Pattern p = Pattern.compile("(\\d+)|([a-z]+)");
+            Matcher m = p.matcher(minusculo);
+            
+            while(m.find())
+            {
+            	String token = m.group(); //pega um token
+            	Integer freq = null;
+            	if(token.length()>1){
+	            	freq = mapPalavrasAtual.get(token); //verifica se esse token ja esta no mapa   
+	            	
+	            	if(filtroTermos!= null) {
+	            		if (filtroTermos.contains(token)){
+	            			if (freq != null) { // se palavra nao existe, insiro com um novo id e freq=1.
+	            				//mapPalavrasAtual.put(token, freq+1);
+	      	                } else{ //se palavra existe, nao faz nada
+	      	                	mapPalavrasAtual.put(token,1);
+	      	            	}
+	      	                
+	            		}
+	            	} 
+	            	else {
+		            	if (freq != null) { // se palavra nao existe, insiro com um novo id e freq=1.
+		            		//mapPalavrasAtual.put(token, freq+1);
+		                } else { //se palavra existe, nao faz nada
+		            		mapPalavrasAtual.put(token,1);
+		            	}
+	            	}
+            	}
+            }
+            
+            //consolita map de presencas
+            consolidaTermoFrequencia(mapPalavrasAtual, mapPalavrasConsolidado);
+        }
+
+        return filtraFrequencia(mapPalavrasConsolidado,minFrequencia);
+    }
+
+	
+	
+    public static String removeNumeros(String passa){
+    	
+    	passa = passa.replaceAll("[1234567890]", "");
+    	return passa;
+    }
+    
+    public static String limpacaracteres(String passa) {
+    		
+    	passa = passa.replaceAll("[ÂÀÁÄÃ]","A");  
+        passa = passa.replaceAll("[âãàáä]","a");  
+        passa = passa.replaceAll("[ÊÈÉË]","E");  
+        passa = passa.replaceAll("[êèéë]","e");  
+        passa = passa.replaceAll("ÎÍÌÏ","I");  
+        passa = passa.replaceAll("îíìï","i");  
+        passa = passa.replaceAll("[ÔÕÒÓÖ]","O");  
+        passa = passa.replaceAll("[ôõòóö]","o");  
+        passa = passa.replaceAll("[ÛÙÚÜ]","U");  
+        passa = passa.replaceAll("[ûúùü]","u");  
+        passa = passa.replaceAll("Ç","C");  
+        passa = passa.replaceAll("ç","c");   
+        passa = passa.replaceAll("[ýÿ]","y");  
+        passa = passa.replaceAll("Ý","Y");  
+        passa = passa.replaceAll("ñ","n");  
+        passa = passa.replaceAll("Ñ","N");  
+        passa = passa.replaceAll("['<>|/]","");  
+        return passa;  
+    	
+    }
+    
+	public static List<String> imprimeFrequencias(Map<String, Integer> mapPalavras) {
+		
+		List<String> output = new ArrayList<String>();
+		
+    	for (Map.Entry<String, Integer> entry : mapPalavras.entrySet()) {
+    		System.out.println(entry.getKey() + ";" + entry.getValue());
+    		output.add(entry.getKey() + ";" + entry.getValue());
+    	}
+    	return output;
+    }
+
+	public static Map<String, Integer> consolidaTermoFrequencia(Map<String, Integer> termoFrequenciaDocumento,
+			Map<String, Integer> termoFrequenciaCategoria) {
+
+		if (termoFrequenciaCategoria.isEmpty()){
+			
+			//para toda chave no mapa do documento
+			for (Map.Entry<String, Integer> entry : termoFrequenciaDocumento.entrySet()) {
+				String termo = entry.getKey();
+				int freqDoc = entry.getValue();
+				termoFrequenciaCategoria.put(termo, freqDoc);
+			}
+			//termoFrequenciaCategoria = termoFrequenciaDocumento;
+		}
+		else 
+		{
+			//para toda chave no mapa do documento
+			for (Map.Entry<String, Integer> entry : termoFrequenciaDocumento.entrySet()) {
+				String termo = entry.getKey();
+				int freqDoc = entry.getValue();
+				//verifica se ela estao no mapa da categoria
+				if (termoFrequenciaCategoria.containsKey(termo))
+				{
+					//se estiver atualiza o valor com a soma dos valores
+					int freqCategoria = termoFrequenciaCategoria.get(termo);
+					termoFrequenciaCategoria.put(termo, freqCategoria+ freqDoc);
+				} else {
+					//se nao estiver adiciona com o valor do documento
+					termoFrequenciaCategoria.put(termo, freqDoc);
+				}
+			}
+			
+		}
+		return termoFrequenciaCategoria;
+
+	}
+    
+}
---
+++ src/util/Util.java
@@ -0,0 +1,173 @@
+package util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+
+public class Util {
+//	private static final String PARAMETERS_PROPERTIES = "parameters.properties";
+	public static DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+
+	/**
+	 * Recebe um mapa que contem a frequencia de cada terno e um termo.
+	 * Retorna o mapa com a frequencia desse termo incrementada 
+	 * @param mapTermoFrequencia
+	 * @param termo
+	 */
+	public static void incrementaFrequencia(Map<String, Integer> mapTermoFrequencia, String termo) {
+		
+		int frequenciaTermo = 0;
+		
+		if (mapTermoFrequencia.containsKey(termo)){
+			frequenciaTermo = mapTermoFrequencia.get(termo);
+		}
+		frequenciaTermo += 1;
+		mapTermoFrequencia.put(termo, frequenciaTermo);
+	}
+	
+	/**
+	 * Recebe o nome do campo e o nome do arquivo de propriedade e retorna o valor referente a esse campo
+	 * @param propertyName
+	 * @param propertiesFileName
+	 * @return
+	 */
+	public static String readFromProperties(String propertyName, String propertiesFileName){
+		File propertiesFile = new File(propertiesFileName);
+		return readFromProperties(propertyName, propertiesFile);
+	}
+	
+	/**
+	 * Recebe o nome do campo e um arquivo de propriedades e retorna o valor referente a esse campo
+	 * @param propertyName
+	 * @param propertiesFile
+	 * @return
+	 */
+	public static String readFromProperties(String propertyName, File propertiesFile){
+		
+		Properties prop = new Properties();
+		try {
+			
+			FileInputStream filePropertiesName = new FileInputStream(propertiesFile.getAbsoluteFile());
+		 	prop.load(filePropertiesName);
+			return prop.getProperty(propertyName);
+		} catch (IOException e) {
+			return null;
+		}
+		
+	}
+	/**
+	 * Recebe dois mapas com a frequencia de NGramas e retorna um map com essas frequencias consolidadas
+	 * 
+	 * @param nGramaFrequencia1
+	 * @param nGramaFrequencia2
+	 */
+	public static Map<String, Integer> consolidaNgramaFrequencia(Map<String, Integer> nGramaFrequencia1,
+			Map<String, Integer> nGramaFrequencia2) {
+		
+		Map<String, Integer> nGramaFrequenciaConsolidado = new HashMap<String, Integer>();
+		
+		if (nGramaFrequencia1 == null || nGramaFrequencia1.size() == 0){
+			return nGramaFrequencia2;
+		}
+		
+		List<String> nGramas1 = new ArrayList<String>();
+		nGramas1.addAll(nGramaFrequencia1.keySet());
+		if (nGramaFrequencia2 != null && nGramaFrequencia2.size() > 0){
+			nGramas1.addAll(nGramaFrequencia2.keySet());
+		}
+		
+		Set<String> nGramas = new HashSet<String>();
+		nGramas.addAll(nGramas1);
+		
+		for (String nGrama : nGramas) {
+			int frequencia = 0;
+			if(nGramaFrequencia1 != null && nGramaFrequencia1.containsKey(nGrama)){
+				frequencia += nGramaFrequencia1.get(nGrama);
+			}
+			if(nGramaFrequencia2 != null && nGramaFrequencia2.containsKey(nGrama)){
+				frequencia += nGramaFrequencia2.get(nGrama);
+			}
+			nGramaFrequenciaConsolidado.put(nGrama, frequencia);
+		}
+		
+		return nGramaFrequenciaConsolidado;
+	}
+
+	public static List<String> removeFreqMin(Map<String, Integer> termosFrequencias, int freqMin) {
+		
+		List<String> removidos = new ArrayList<String>();
+		for (Entry<String, Integer>  termoFrequencia : termosFrequencias.entrySet()) {
+			if(termoFrequencia.getValue() < freqMin){
+				removidos.add(termoFrequencia.getKey());
+				//termosFrequencias.remove(termoFrequencia);
+			}
+		}
+		
+		return removidos;
+		
+	}
+	
+	public static Boolean getBoolean(String string) {
+		
+		return string.contentEquals("1");
+	}
+	
+	public static List<String> splitToArray(String file, String separador){
+		String[] parts = file.split(separador);			
+		List<String> arrayParts = new ArrayList<String>();
+		for(String part : parts){
+			arrayParts.add(part);
+		}		
+		return arrayParts;
+	}
+	
+	public static String[] limpaTokens(String[] tokens) {
+		
+		List<String> tokensArray = new ArrayList<String>();
+		for (String token : tokens) {
+			if (!token.trim().contentEquals("")){
+				tokensArray.add(token);
+			}
+		}
+		
+		String [] tokensFiltrados = new String[tokensArray.size()];
+		for (int i = 0; i < tokensArray.size(); i++) {
+			tokensFiltrados[i] = tokensArray.get(i);
+		}
+		
+		return tokensFiltrados;
+	}
+	
+    public static String limpacaracteres(String passa) {
+		
+    	passa = passa.replaceAll("[ÂÀÁÄÃ]","A");  
+        passa = passa.replaceAll("[âãàáä]","a");  
+        passa = passa.replaceAll("[ÊÈÉË]","E");  
+        passa = passa.replaceAll("[êèéë]","e");  
+        passa = passa.replaceAll("ÎÍÌÏ","I");  
+        passa = passa.replaceAll("îíìï","i");  
+        passa = passa.replaceAll("[ÔÕÒÓÖ]","O");  
+        passa = passa.replaceAll("[ôõòóö]","o");  
+        passa = passa.replaceAll("[ÛÙÚÜ]","U");  
+        passa = passa.replaceAll("[ûúùü]","u");  
+        passa = passa.replaceAll("Ç","C");  
+        passa = passa.replaceAll("ç","c");   
+        passa = passa.replaceAll("[ýÿ]","y");  
+        passa = passa.replaceAll("Ý","Y");  
+        passa = passa.replaceAll("ñ","n");  
+        passa = passa.replaceAll("Ñ","N");  
+        passa = passa.replaceAll("['<>|/]","");  
+        return passa;  
+    	
+    }
+}
